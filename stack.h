664ceb7afb747da03ffb582ee845422803b850af
kenshen112 <finalfantasytimothy@gmail.com>
2019-01-17 20:28:57 -0700

    Added basic class structure

--- stack.h
+++ stack.h
@@ -1,9 +1,72 @@
 #ifndef STACK_H
 #define STACK_H
 
-class stack
+template <class T>
+
+class Stack {
+
+private:
+  T *data;
+  int numElements;
+  int numCapacity;
+
+public:
+  Stack() 
+  {
+    numElements = 0;
+    numCapacity = 0;
+    data = nullptr;
+  }
+
+  Stack(int numCap) 
+  {
+    numElements = 0;
+    numCapacity = numCap;
+  }
+
+  Stack(Stack &rhs) 
+  {
+
+  }
+
+  void assign(Stack &rhs) {}
+
+  int size() 
+  {
+
+  }
+
+  int capacity()
+  {
+
+  }
+
+bool empty()
 {
 
+}
+
+void clear()
+{
+
+}
+
+
+void push(T data)
+{
+
+
+
+}
+
+
+void pop()
+{
+
+}
+
+
+  ~Stack();
 };
 
 #endif /* STACK_H */
